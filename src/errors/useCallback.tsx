ba98424fc4f343d2e438640ef1976ef07497b1dc
Tyson Skiba <tyson@skiba.com.au>
2023-01-15 00:01:09 +1100

    fix: Genrics in useCallback

--- src/errors/useCallback.tsx
+++ src/errors/useCallback.tsx
@@ -1,13 +1,11 @@
 import { useCallback as useCallbackReact, useState, DependencyList } from 'react';
 
-type CallBackReturn<T extends (...args: any[]) => any> = ReturnType<typeof useCallbackReact<T>>;
-
-export function useCallback<T extends (...args: any[]) => any>(callback: T, deps: DependencyList): CallBackReturn<T>{
+export function useCallback<T extends (...args: any[]) => any>(callback: T, deps: DependencyList): T {
     const [error, setError] = useState<Error>();
 
     if (error) throw error;
     
-    return useCallback(((...args: any) => {
+    return useCallbackReact(((...args: any) => {
         try {
             const result = callback(...args);
             Promise.resolve(result).catch((err) => setError(err));
